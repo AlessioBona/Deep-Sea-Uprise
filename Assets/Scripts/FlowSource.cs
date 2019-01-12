8814fca2a41a6f0c76cd25a7f20a5ab8153c9a11
Christopher Mielack <christopher.mielack@me.com>
2019-01-12 16:25:21 +0100

    wip

--- Assets/Scripts/FlowSource.cs
+++ Assets/Scripts/FlowSource.cs
@@ -1,11 +1,22 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(TurretScript))]
 public class FlowSource : MonoBehaviour
 {
     public float FlowStrength = 1;
+    private float _actualFlowStrength;
+    private TurretScript _turretScript;
     void Start()
     {
         FluidSimManager.Instance.FlowSources.Add(this);
+        _turretScript = GetComponent<TurretScript>();
+        _actualFlowStrength = FlowStrength;
+    }
+
+    void Update()
+    {
+        // TODO if is being positioned
+        FlowStrength = false ? 0 : _actualFlowStrength;
     }
 
     private void OnDestroy()
