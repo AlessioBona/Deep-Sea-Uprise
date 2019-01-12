38f9314d13bb5844f2c087ecfda4c9e51faabb6a
AlessioBona <alefbona@gmail.com>
2019-01-12 17:01:50 +0100

    StartScene and LevelOne

---
+++ Assets/Scripts/MyGUIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MyGUIManager : MonoBehaviour {
+
+    [SerializeField]
+    TextMeshProUGUI timerGUI;
+
+    [SerializeField]
+    string timerGUIString;
+
+    [SerializeField]
+    GameObject startObject;
+
+    float timer = 0;
+    bool timerOn = false;
+
+	// Use this for initialization
+	void Start () {
+        timerGUI.text = timerGUIString + " 0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timerOn) {
+            timer = timer + Time.deltaTime;
+            timerGUI.text = timerGUIString + " " + System.Math.Round(timer, 0);
+                }
+	}
+
+    public void StartLevelOne()
+    {
+        SceneManager.LoadScene("LevelOne", LoadSceneMode.Additive);
+        startObject.SetActive(false);
+        timerOn = true;
+    }
+}
