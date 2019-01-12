de51a14aef2f83b65bb1ca0e82874bd768f2a4a4
Christopher Mielack <christopher.mielack@me.com>
2019-01-12 15:23:51 +0100

    added fish with behaviour + prefab

---
+++ Assets/FishBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FishBehaviour : MonoBehaviour
+{
+	public GameObject Target;
+
+	private Rigidbody _rigidbody;
+	public float TurningForce;
+
+
+	private float lastTimePulsed;
+	public float PulseFrequency = 1;
+	public float PulseStrength = 10;
+	public bool DoPulse = false;
+	void Start()
+	{
+		_rigidbody = GetComponent<Rigidbody>();
+		lastTimePulsed = Time.realtimeSinceStartup;
+	}
+	
+	void Update()
+	{
+		// TODO swim forward in bursts
+		TurnTowardTarget();
+		PulseForward();
+	}
+
+	void TurnTowardTarget()
+	{
+		var forward = transform.forward;
+		var targetDirection = Target.transform.position - transform.position;
+		var torqueDirection = Vector3.Cross(forward, targetDirection);
+		_rigidbody.AddTorque(torqueDirection*TurningForce);
+	}
+
+	void PulseForward()
+	{
+		if (!DoPulse || Time.realtimeSinceStartup - lastTimePulsed > PulseFrequency)
+		{
+			lastTimePulsed = Time.realtimeSinceStartup;
+			_rigidbody.AddForce(transform.forward*PulseStrength);
+		}
+	}
+	
+}
--- Assets/Scripts/FlowSource.cs
+++ Assets/Scripts/FlowSource.cs
@@ -3,11 +3,9 @@
 public class FlowSource : MonoBehaviour
 {
     public float FlowStrength = 1;
-    public Rigidbody Rigidbody;
     void Start()
     {
         FluidSimManager.Instance.FlowSources.Add(this);
-        Rigidbody = GetComponent<Rigidbody>();
     }
 
     private void OnDestroy()
