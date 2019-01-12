63e33fc62fc5a3ec7c1bec62f877dacb7f277f0c
AlessioBona <alefbona@gmail.com>
2019-01-12 14:28:24 +0100

    Some Stuff

--- Assets/Scripts/TurretScript.cs
+++ Assets/Scripts/TurretScript.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurretScript : MonoBehaviour {
+public class TurretScript : MonoBehaviour{
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -13,4 +13,5 @@ public class TurretScript : MonoBehaviour {
 	void Update () {
 		
 	}
+
 }
