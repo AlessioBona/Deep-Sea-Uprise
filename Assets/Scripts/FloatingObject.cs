35ce4e3b239fb6b96fc80539a661fda99bf35a30
Christopher Mielack <christopher.mielack@me.com>
2019-01-12 14:11:17 +0100

    flow source script

---
+++ Assets/Scripts/FloatingObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingObject : MonoBehaviour
+{
+    public float weight = 1f;
+
+    void Update()
+    {
+        //Every frame this object will move by velocity at point * (1/weight)
+
+    }
+}
---
+++ Assets/Scripts/FlowSource.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class FlowSource : MonoBehaviour
+{
+    public float FlowStrength = 1;
+    public Rigidbody Rigidbody;
+    void Start()
+    {
+        FluidSimManager.Instance.FlowSources.Add(this);
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnDestroy()
+    {
+        FluidSimManager.Instance.FlowSources.Remove(this);
+    }
+}
